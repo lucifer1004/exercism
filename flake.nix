ab79fc57775906f4a740a26efd2aa363cc19296e
Gabriel Wu <qqbbnease1004@126.com>
2024-06-26 15:55:52 +0800

    [clojure] init

--- flake.nix
+++ flake.nix
@@ -12,12 +12,17 @@
     {
       devShells.x86_64-linux.default = pkgs.mkShell {
         buildInputs = [
+          # OcaML
           pkgs.ocaml
           pkgs.ocamlPackages.dune_3
           pkgs.ocamlPackages.findlib
           pkgs.ocamlPackages.lsp
           pkgs.ocamlPackages.ounit2
           pkgs.ocamlPackages.ocamlformat
+
+          # Clojure
+          pkgs.clojure
+          pkgs.leiningen
         ];
 
         shellHook = ''

950be4b8a1918b9fbd2d6f9bfb9a3932db5e9502
Gabriel Wu <13583761+lucifer1004@users.noreply.github.com>
2025-11-11 20:24:52 +0800

    [python] (update)

---
+++ python/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Python environment for Exercism";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.python314
+          pkgs.python314Packages.pytest
+        ];
+      };
+    };
+}
+

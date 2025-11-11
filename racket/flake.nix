d428d14220d31705edd957b4f0a1cfb513bad487
Gabriel Wu <13583761+lucifer1004@users.noreply.github.com>
2025-11-11 20:17:31 +0800

    [racket] (update)

---
+++ racket/flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Racket environment for Exercism";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [ pkgs.racket ];
+      };
+    };
+}

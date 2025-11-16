ad8676dd064dc4fc30928c04be8c80f00a65141a
Gabriel Wu <13583761+lucifer1004@users.noreply.github.com>
2025-11-16 23:55:33 +0800

    [racket] leap

---
+++ racket/leap/leap.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+(provide leap-year?)
+
+(define (leap-year? year)
+  (cond
+    [(zero? (modulo year 400)) #t]
+    [(zero? (modulo year 100)) #f]
+    [(zero? (modulo year 4)) #t]
+    [else #f]))

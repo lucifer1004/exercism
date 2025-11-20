b48bc0353b0e3612a3acc1be2925359e8ad9bfcf
Gabriel Wu <13583761+lucifer1004@users.noreply.github.com>
2025-11-20 14:43:13 +0800

    [racket,raku,rust,swift,wasm,zig] add format commands

--- wasm/eslint.config.js
+++ wasm/eslint.config.js
@@ -1,26 +1,25 @@
-import exercismConfig from '@exercism/eslint-config-javascript';
+import exercismConfig from "@exercism/eslint-config-javascript";
 
 export default [
   ...exercismConfig,
   {
-    files: ['**/*.js', '**/*.spec.js'],
+    files: ["**/*.js", "**/*.spec.js"],
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+      ecmaVersion: "latest",
+      sourceType: "module",
       globals: {
         // Node.js globals
-        process: 'readonly',
+        process: "readonly",
         // Jest globals
-        describe: 'readonly',
-        test: 'readonly',
-        expect: 'readonly',
-        beforeEach: 'readonly',
-        afterEach: 'readonly',
-        beforeAll: 'readonly',
-        afterAll: 'readonly',
-        jest: 'readonly',
+        describe: "readonly",
+        test: "readonly",
+        expect: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+        beforeAll: "readonly",
+        afterAll: "readonly",
+        jest: "readonly",
       },
     },
   },
 ];
-
